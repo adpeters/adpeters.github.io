50f55de1615e04e5a2ab6175b1e2270fb72916c9
Alec Peters <alec.peters@gmail.com>
2020-10-13 23:39:59 -0400

    removed jumbotron, updated styling of page title, added horiz. rules

--- pages/home.js
+++ pages/home.js
@@ -5,26 +5,30 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Home = () => {
     return (
-        <div className="container bg-white page">
+        <div className="container page">
             <div className="row">
-                <div className="jumbotron w-100">
-                    <h1>Alec Peters</h1>
-                    <p>Freelance software developer and data scientist</p>
+                <div className="col pt-5">
+                    <h1 className="display-2 page-title">Alec Peters</h1>
+                    <p className="text-muted">Freelance software developer and data scientist</p>
                 </div>
             </div>
+            <hr />
             <div className="row">
                 <div className="col">
-                    <p>Alec is a software developer located in Portland, Oregon specializing in web development and data engineering. He is currently available for consulting work.</p>
+                    <p className="lead">Alec is a software developer located in Portland, Oregon specializing in web development and data engineering. He is currently available for consulting work.</p>
                 </div>
             </div>
             <footer className="footer row p-3">
-                <div className="col-sm-1">
+                <div className="col-md-12">
+                    <hr />
+                </div>
+                <div className="col-xs-1 col-sm-2 col-md-1">
                     <a href="https://github.com/adpeters" className="icon" title="Alec Peters' GitHub profile">
                         <FontAwesomeIcon icon={faGithub} size="2x" />
                         <p className="text-center">Github</p>
                     </a>
                 </div>
-                <div className="col-sm-1">
+                <div className="col-xs-1 col-sm-2 col-md-1">
                     <a href="mailto:alec.peters@gmail.com" className="icon" title="Send an email to Alec">
                         <FontAwesomeIcon icon={faEnvelope} size="2x" />
                         <p className="text-center">Email</p>
