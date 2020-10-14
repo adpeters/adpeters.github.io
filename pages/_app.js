aa434598f2e978ad619e0f6864bc6c6d4de520e8
Alec Peters <alec.peters@gmail.com>
2020-10-14 18:00:27 -0400

    fixed fontawesome css styles not being imported

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-svg-core/styles.css' // Import the CSS
 import '../styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+
+config.autoAddCss = false;
 
 export default function MyApp({ Component, pageProps}) {
     return <Component {...pageProps} />
